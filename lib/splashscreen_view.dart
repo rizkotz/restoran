060ddb08a0d1fa035e8fc95911e02eea0cadda2b
rizkotz <rizqifauzan28@gmail.com>
2022-06-28 21:05:38 +0700

    splashscreen

---
+++ lib/splashscreen_view.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:restoran/home/home_view.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({Key? key}) : super(key: key);
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    openSplashScreen();
+  }
+
+  openSplashScreen() async {
+    //bisa diganti beberapa detik sesuai keinginan
+    var durasiSplash = const Duration(seconds: 2);
+    return Timer(durasiSplash, () {
+      //pindah ke halaman home
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
+        return HomePage();
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          "assets/images/splash.png",
+          width: 200,
+          height: 88,
+        ),
+      ),
+    );
+  }
+}
