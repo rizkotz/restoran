f9d7d82eaf6b460caf0df8dea06a5c2c9bb6bc19
rizkotz <rizqifauzan28@gmail.com>
2022-06-29 19:17:38 +0700

    gambar tidak muncul

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -176,7 +176,7 @@ class HomePage extends StatelessWidget {
                         child: Row(
                           children: [
                             Image(
-                              image: AssetImage("assets/images/makanan.jpeg"),
+                              image: AssetImage("assets/imagess/makanan.jpeg"),
                               height: 200,
                               width: 200,
                             ),
@@ -188,7 +188,7 @@ class HomePage extends StatelessWidget {
                             ),
                             Image(
                               image: AssetImage(
-                                  "assets/images/beverages_icon.png"),
+                                  "assets/imagess/beverages_icon.png"),
                               height: 200,
                               width: 200,
                             ),
@@ -207,7 +207,8 @@ class HomePage extends StatelessWidget {
                         child: Row(
                           children: [
                             Image(
-                              image: AssetImage("assets/images/snack_icon.png"),
+                              image:
+                                  AssetImage("assets/imagess/snack_icon.png"),
                               height: 200,
                               width: 200,
                             ),
@@ -218,7 +219,7 @@ class HomePage extends StatelessWidget {
                                   fontSize: 14, fontWeight: FontWeight.bold),
                             ),
                             Image(
-                              image: AssetImage("assets/images/taco_icon.jpg"),
+                              image: AssetImage("assets/imagess/taco_icon.jpg"),
                               height: 200,
                               width: 200,
                             ),
