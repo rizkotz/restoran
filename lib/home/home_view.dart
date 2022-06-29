98e362657198ca42b8d1c7d343b8b33d40c2a7c8
NaufaAlfi <naufalalfi100@gmail.com>
2022-06-29 10:11:21 +0700

    pokok lanjutan

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: deprecated_member_use, unused_element
+// ignore_for_file: deprecated_member_use, unused_element, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 
@@ -39,6 +39,55 @@ class HomePage extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Widget _selectedExtras(@required String name) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(color: Colors.grey, width: 2)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 60,
+            decoration: BoxDecoration(),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            name,
+            style: TextStyle(
+              fontSize: 17,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _selectedFrequency(
+      {required Color color,
+      required String tittle,
+      required BoxBorder border,
+      required TextStyle textStyle}) {
+    return Container(
+      height: 45,
+      width: 120,
+      margin: EdgeInsets.symmetric(horizontal: 7, vertical: 20),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(7),
+        border: border,
+      ),
+      child: Center(
+        child: Text(
+          tittle,
+          style: textStyle,
+        ),
       ),
     );
   }
@@ -106,34 +155,92 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 30,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Selected Extas',
-                        style: TextStyle(fontSize: 20, color: Colors.black),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 20),
-                        child: Container(
-                          height: 300,
-                          child: GridView.count(
-                            crossAxisCount: 2,
-                            crossAxisSpacing: 12,
-                            mainAxisSpacing: 8,
-                            childAspectRatio: 1.30,
-                            children: [
-                              ///
-                            ],
-                          ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 30,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Selected Extas',
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Container(
+                        height: 300,
+                        child: GridView.count(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 12,
+                          mainAxisSpacing: 8,
+                          childAspectRatio: 1.30,
+                          children: [
+                            //_selectedExtras(),
+                            //_selectedExtras(),
+                            //_selectedExtras(),
+                            //_selectedExtras(),
+                          ],
                         ),
                       ),
-                    ],
-                  )),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 13),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 13),
+                      child: Text("Selected Frequensi",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            letterSpacing: 1,
+                            fontWeight: FontWeight.w400,
+                          )),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _selectedFrequency(
+                            tittle: 'Weekly',
+                            color: Colors.white,
+                            border: Border.all(
+                                color: Theme.of(context).accentColor),
+                            textStyle:
+                                TextStyle(color: Theme.of(context).accentColor),
+                          ),
+                          _selectedFrequency(
+                            tittle: 'BI-Weakly',
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey),
+                            textStyle: TextStyle(color: Colors.black),
+                          ),
+                          _selectedFrequency(
+                            tittle: 'Monthly',
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey),
+                            textStyle: TextStyle(color: Colors.black),
+                          ),
+                          _selectedFrequency(
+                            tittle: 'Yearly',
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey),
+                            textStyle: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
